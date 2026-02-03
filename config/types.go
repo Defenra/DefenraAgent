8070076b402f44fc054afc7011a9e1903f893629
tuesday <39628283+Rxflex@users.noreply.github.com>
2026-02-03 02:11:21 +0100

    feat(proxy): implement HTTP to HTTPS redirect

--- config/types.go
+++ config/types.go
@@ -142,11 +142,12 @@ type Slowloris struct {
 }
 
 type SSL struct {
-	Enabled        bool   `json:"enabled"`
-	EncryptionMode string `json:"encryptionMode"` // off, flexible, full, full_strict
-	Certificate    string `json:"certificate"`
-	PrivateKey     string `json:"privateKey"`
-	AutoRenew      bool   `json:"autoRenew"`
+	Enabled             bool   `json:"enabled"`
+	EncryptionMode      string `json:"encryptionMode"` // off, flexible, full, full_strict
+	Certificate         string `json:"certificate"`
+	PrivateKey          string `json:"privateKey"`
+	AutoRenew           bool   `json:"autoRenew"`
+	HTTPRedirectToHTTPS bool   `json:"httpRedirectToHttps"`
 }
 
 type Proxy struct {
